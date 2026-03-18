c4fb492a5300a334edddcac10aa01c929501a1f1
Masrafi Siam <mesrafi.siam@gmail.com>
2026-03-18 14:03:35 +0600

    TEsting board

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
+# main.py
 
 from board import Board
 
+
 def main():
     board = Board()
+
+    print("Initial Board:")
     board.print_board()
+
+    print("\nSwitching turn...")
+    board.switch_turn()
+    board.print_board()
+
+    print("\nSwitching turn again...")
+    board.switch_turn()
+    board.print_board()
+
 
 if __name__ == "__main__":
     main()
